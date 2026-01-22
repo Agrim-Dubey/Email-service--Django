ad7d800565a9efe3699b2e105ae9f3ba0b715c5c
Agrim <agrimdubey37@gmail.com>
2026-01-22 08:19:31 +0530

    Changes in models and the serializers

--- apps/users/models.py
+++ apps/users/models.py
@@ -3,16 +3,14 @@ from django.db import models
 # Create your models here.
 
 
-class User(models.Model):
+class Student(models.Model):
     student_name=models.CharField(unique=False,null=False,max_length=30,default = "noname")
     student_number=models.IntegerField(unique=True,null=False)
     student_mail = models.EmailField(unique=True,blank=False)
     created_at = models.DateTimeField(auto_now_add=True)
-    verification_string = models.CharField(null=False,unique= True,max_length=255)
-    is_verified = models.BooleanField(default=False)
 
     def __str__(self):
-            return self.student_mail
+            return f"{self.student_name} with the email : {self.student_mail}"
         
         
         
--- apps/users/serializers.py
+++ apps/users/serializers.py
@@ -1,13 +1,17 @@
 # serializers.py
 from rest_framework import serializers
-from .models import User
-import uuid
+from .models import Student
 
-class UserSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = User
-        fields = ("student_name","student_number","student_mail")
-
-    def create(self, validated_data):
-        validated_data["verification_token"] = uuid.uuid4().hex
-        return super().create(validated_data)
+class StudentSerializer(serializers.ModelSerializer):
+    class Meta : 
+        model = Student
+        fields = ['id','name','student_number','email','created_at']
+    
+    def validate_email(self,value):
+        if Student.objects.filter(email=value).exists():
+            raise serializers.ValidationError("Student with this email has already been registered")
+        return value
+    def validate_student_number(self,value):
+        if Student.objects.filter(student_number =  value).exists():
+            raise serializers.ValidationError("Student with this student number has already been registered")
+        return value
